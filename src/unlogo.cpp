6271929006cf3951bb699c56e9b2f72173e60020
jefftimesten <jefftimesten@e6005f03-ab88-5018-ef88-7cbc062004f6>
2010-09-02 16:24:27 +0000

    Added support for normalized coordinate parameters

--- src/unlogo.cpp
+++ src/unlogo.cpp
@@ -20,16 +20,15 @@
 using namespace unlogo;
 
 
-Image prev;
-int framenum=0;
-int targetframe;
-int inititalFeatures;
-double initialArea;
-bool targetFound;
+Image prev;						// The last frame -- for optical flow.
+int framenum=0;					// Current frame number
+int targetframe;				// The frame number on which the target logo appears
+int inititalFeatures;			// The number of feaatures in the target frame/bounds
+double initialArea;				// The initial area of the black box
+bool targetFound;				// Have we gotten to the target and is it still visible?
+bool coordsNormalized=false;	// Did the coords come in normalized?
+Mat contour;					// Black box contour
 
-// Do we really need the points in all of these different formats?
-Mat contour;
-Point corners[4];
 
 extern "C" int init( const char* argstr )
 {
@@ -40,8 +39,9 @@ extern "C" int init( const char* argstr )
 		// Parse arguments.
 		vector<string> argv = split(argstr, ":");
 		int argc = argv.size();
-		if(argc < 9) {
-			log(LOG_LEVEL_ERROR, "You must supply 9 arguments.");
+		if(argc < 9)
+		{
+			log(LOG_LEVEL_ERROR, "You must supply at least 9 arguments.");
 			exit(-1);
 		}
 		
@@ -49,14 +49,25 @@ extern "C" int init( const char* argstr )
 		targetframe = atol(argv[0].c_str());
 		contour = Mat(4, 1, CV_32FC2);  // 4row 1col 2channel matrix
 		int i,j;
+		float x, y;
+		float total;
 		for(i=0,j=1; i<4; i++,j+=2)
 		{
+			x = atof(argv[j+0].c_str());
+			y = atof(argv[j+1].c_str());
 			float* ptr = contour.ptr<float>(i);
-			ptr[0] = (float)atol(argv[j+0].c_str());
-			ptr[1] = (float)atol(argv[j+1].c_str());
-			corners[i] = Point(ptr[0], ptr[1]);
+			ptr[0] = x;
+			ptr[1] = y;
+			total += x;	
 		}
 
+		// Ghetto way of determining whether coordinates are normalized to
+		// maintain backwards compatability.
+		if(total < 4.)
+		{
+			coordsNormalized=true;
+		}
+		
 		// Get the hull of the points provided so we don't fail right away if the points are convex
 		vector<Point2f> hull;
 		convexHull(contour, hull, true); 
@@ -161,17 +172,23 @@ extern "C" int process( uint8_t* dst[4], int dst_stride[4],
 
 	if(targetFound)
 	{
+		Point corners[4];
+		
 		// Move corner points fowards the homography points
 		for(int i=0; i<4; i++)
 		{
 			float* ptr = contour.ptr<float>(i);
-			Point flow(ptr[0], ptr[1]);
-			corners[i] = flow;
+			corners[i] = Point(ptr[0], ptr[1]);
+			if(coordsNormalized)
+			{
+				corners[i].x += width;
+				corners[i].y *= height;
+			}
 			//lerp(corners[i], flow, 1.8);
 		}
 		
 		// Draw the rect.
-		fillConvexPoly(input.cvImage, corners, 4, CV_RGB(0,0,0));	
+		fillConvexPoly(input.cvImage, corners, 4, CV_RGB(0,0,0));
 	}
 
 	Image output(width, height, dst[0], dst_stride[0]);			// point the 'output' image to the FFMPEG data array	
